6a8e57d038cb2d9f61a4602f01359ba034ff8f8b
Kit Burton-Senior <mail@kitbs.com>
2020-03-25 15:57:42 +0000

    add custom html

--- resources/js/components/Url.vue
+++ resources/js/components/Url.vue
@@ -2,7 +2,19 @@
         <div>
             <a :href="field.value"
                 :title="field.title || field.value"
-                v-if="field.value && clickable"
+                v-if="hasCustomHtml && clickable"
+                class="cursor-pointer no-underline text-90"
+                :target="field.sameTab ? '_self' : '_blank'"
+                :rel="field.rel"
+                v-html="field.customHtml"
+            >
+            </a>
+            <div v-else-if="hasCustomHtml && !clickable"
+                v-html="field.customHtml">
+            </div>
+            <a :href="field.value"
+                :title="field.title || field.value"
+                v-else-if="field.value && clickable"
                 class="cursor-pointer dim no-underline text-primary flex items-center"
                 :target="field.sameTab ? '_self' : '_blank'"
                 :rel="field.rel"
@@ -17,10 +29,12 @@
                     <path d="M19 6.41L8.7 16.71a1 1 0 1 1-1.4-1.42L17.58 5H14a1 1 0 0 1 0-2h6a1 1 0 0 1 1 1v6a1 1 0 0 1-2 0V6.41zM17 14a1 1 0 0 1 2 0v5a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V7c0-1.1.9-2 2-2h5a1 1 0 0 1 0 2H5v12h12v-5z"/>
                 </svg>
 
-                <span class="block truncate">
+                <span v-if="shouldDisplayAsHtml && field.label" class="block" v-html="field.label"></span>
+                <span v-else class="block truncate">
                     {{ field.label ? field.label : field.value }}
                 </span>
             </a>
+            <span v-else-if="shouldDisplayAsHtml && field.label" class="block" v-html="field.label"></span>
             <span v-else-if="field.value" class="block truncate">{{ field.value }}</span>
             <span v-else>&mdash;</span>
         </div>
@@ -29,5 +43,14 @@
 <script>
 export default {
     props: ['resource', 'resourceName', 'resourceId', 'field', 'clickable'],
+
+    computed: {
+        hasCustomHtml() {
+            return !!this.field.customHtml
+        },
+        shouldDisplayAsHtml() {
+            return this.field.asHtml
+        }
+    }
 }
 </script>
